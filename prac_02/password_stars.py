c81a5ae2e6f26dc15fed02c41ee3f887d38e75c3
JocoBuen <josephconrad.buen@my.jcu.edu.au>
2022-09-26 21:17:13 +1000

    Adding correct format

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -5,8 +5,14 @@ while length of password < 6 or blank in password
 star_password = length of password * "*"
 display star_password
 """
-users_password = input("Enter password: ")
-while len(users_password) < 6 or " " in users_password:
-    print("Invalid Password")
+
+
+def main():
     users_password = input("Enter password: ")
-print("*" * len(users_password))
+    while len(users_password) < 6 or " " in users_password:
+        print("Invalid Password")
+        users_password = input("Enter password: ")
+    print("*" * len(users_password))
+
+
+main()
