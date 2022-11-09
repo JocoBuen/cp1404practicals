75a0f2a96edfca7f573c52c91eb7ab88b4f58575
jocobuen <josephconrad.buen@my.jcu.edu.au>
2022-11-09 22:22:51 +1000

    Refactor guitars program to use function

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,6 +1,7 @@
 CURRENT_YEAR = 2022
 VINTAGE_YEAR = 50
 
+
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
@@ -16,3 +17,6 @@ class Guitar:
 
     def is_vintage(self):
         return self.get_age() >= VINTAGE_YEAR
+
+    def __lt__(self, other):
+        return self.year < other.year
