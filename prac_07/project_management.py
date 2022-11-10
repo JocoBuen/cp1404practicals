c067264f9afe6e14f6698e61ac26ba51f29cfa2d
jocobuen <josephconrad.buen@my.jcu.edu.au>
2022-11-10 10:45:42 +1000

    Complete loading and first part of display projects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,5 +1,6 @@
 from prac_07.project import Project
 
+FILENAME = 'projects.txt'
 MENU = """- (L)oad projects
 - (S)ave projects
 - (D)isplay projects
@@ -8,26 +9,25 @@ MENU = """- (L)oad projects
 - (U)pdate project
 - (Q)uit"""
 
-# projects = []
-# in_file = open(FILENAME, 'r')
-# in_file.readline()
-# for line in in_file:
-#     parts = line.strip().split(',')
-#     projects.append(parts)
-# in_file.close()
-#
-# for project in projects:
-#     print(project)
+projects = []
 
 print(MENU)
 choice = input(">>> ").lower()
 while choice != "q":
     if choice == "l":
-        pass
+        in_file = open(FILENAME, 'r')
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split('\t')
+            project_to_add = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+            projects.append(project_to_add)
+        in_file.close()
+
     elif choice == "s":
         pass
     elif choice == "d":
-        pass
+        for project in projects:
+            print(project)
     elif choice == "f":
         pass
     elif choice == "a":
